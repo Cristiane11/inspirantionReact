25d520ac8a466b9d80ee2cb1a934dca6344e2a7f
Cristiane11 <bjwmcd@yahoo.com>
2020-12-10 15:53:35 -0500

    update

--- natours-react/src/App.js
+++ natours-react/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+//import {Router} from 'react-router-dom';
+//import Home from './componets/Home';
+//import Header from './componets/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ natours-react/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import '../App.css';
+
+function Header() {
+  return (
+            <div>
+            <header class="header">
+                <div class ="logo-box">
+                     </div>
+                        <div class="text-box">
+                        <h1 class="heading-primary">
+                        <span class="heading-primary-main"> Outdoors</span> 
+                        <span class="heading-primary-sub"> Is Where Life Happens </span>
+                    </h1>
+                <button>DISCOVER OUR TOURS</button>
+                </div>
+            </header>
+
+    </div>
+  );
+}
+
+export default Header;
